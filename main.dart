e0489e57efc113aa672f777852fe636a05bd17a5
Diya Wadhwani <54880903+DiyaWadhwani@users.noreply.github.com>
2020-04-18 20:46:17 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:metrorun/all_screens/front.dart';
+
+
+void main() {
+
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'MetroRun',
+        home: Front(),
+      )
+  );
+}
